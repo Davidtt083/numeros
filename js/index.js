863fd79915679dec59c88ab497c65952a40c06b0
Davidtt083 <92117303+Davidtt083@users.noreply.github.com>
2022-09-21 23:04:38 -0500

    uno

---
+++ js/index.js
@@ -0,0 +1,75 @@
+
+
+
+    function addClassToImage(element) {
+        var innerImage = document.getElementById('ima01');
+      if(innerImage) {
+          if(innerImage.classList.contains('clicked')) {
+        innerImage.classList.remove('clicked');
+        }
+        else {
+              innerImage.classList.add('clicked');
+        }
+      }
+    }
+
+    
+    function addClassToImage2(element) {
+        var innerImage = document.getElementById('ima02');
+      if(innerImage) {
+          if(innerImage.classList.contains('clicked')) {
+        innerImage.classList.remove('clicked');
+        }
+        else {
+              innerImage.classList.add('clicked');
+        }
+      }
+    }
+
+    function addClassToImage3(element) {
+        var innerImage = document.getElementById('ima03');
+      if(innerImage) {
+          if(innerImage.classList.contains('clicked')) {
+        innerImage.classList.remove('clicked');
+        }
+        else {
+              innerImage.classList.add('clicked');
+        }
+      }
+    }
+
+    function addClassToImage4(element) {
+        var innerImage = document.getElementById('ima04');
+      if(innerImage) {
+          if(innerImage.classList.contains('clicked')) {
+        innerImage.classList.remove('clicked');
+        }
+        else {
+              innerImage.classList.add('clicked');
+        }
+      }
+    }
+
+    function addClassToImage5(element) {
+        var innerImage = document.getElementById('ima05');
+      if(innerImage) {
+          if(innerImage.classList.contains('clicked')) {
+        innerImage.classList.remove('clicked');
+        }
+        else {
+              innerImage.classList.add('clicked');
+        }
+      }
+    }
+
+    function addClassToImage6(element) {
+        var innerImage = document.getElementById('ima06');
+      if(innerImage) {
+          if(innerImage.classList.contains('clicked')) {
+        innerImage.classList.remove('clicked');
+        }
+        else {
+              innerImage.classList.add('clicked');
+        }
+      }
+    }
